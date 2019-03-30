0a271e4847b244a9e7a6884dd08977a54a674099
Brayan <cdn.eem_1@comunidad.unam.mx>
2019-03-30 17:41:56 -0600

    Readme y archivo de configuración modificado

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# KSI-Mart
+#KSI-Mart
+
 Una tienda web para el curso de  Desarrollo de Software
+###Requerimientos
+------------
+
+Se necesita:
+- Tener instalado docker
+- Tener habilitado el docker-compose, en caso de no tenerlo [en este enlace](https://docs.docker.com/compose/install/) se puede revisar como.
+
+###Instrucciones
+
+------------
+
+- Al descargar este repositorio con `git clone` o `git remote add `  debera abrir la terminal y colocarse justo donde se encuentre el archivo **docker-compose.yml**
+- Posteriormente ejecutar el comando `docker-compose up -d` , este proceso podrá tardar algunos minutos. Favor de ignorar los errores xd
+- Despues de que se haya terminado la instalación deberemos incursionarnos dentro del contenedor de mysql para cargar la base de datos.
+- Para ello realizamos el comando `docker exec -it mysql bash` esto nos permitira ingresar a la bash del contenedor
+- Ahora adentro debemos ingresar el siguiente comando para cargar la base de datos `mysql -u root -p KsiMart < /home/KsiMart_MYSQL.sql`
+- nos pedirá la contraseña la cual es:  **1q2w3e4r**
+- comando para llenar los datos `mysql -u root -p KsiMart < /home/KsiMart_carga_inicial.sql`
+- nos pedirá la contraseña nuevamente:  **1q2w3e4r**
+- Ahora podemos salir del contenedor con `CTRL+p+q`
+- Finalmente pueden visualizar la pagina inicial en el siguiente enlace-> [KSI-Mart](http://localhost:80 "KSI-Mart")
+
+###End
--- public_html/mysql/KsiMart_MYSQL.sql
+++ public_html/mysql/KsiMart_MYSQL.sql
@@ -1,47 +1,47 @@
-
-create table tienda (
-	id_tienda integer auto_increment,
-	sucursal varchar (100) not null,
-	direccion varchar (100) not null,
+create table tienda(
+	id_tienda int ,
+	sucursal varchar(100) not null,
+	direccion varchar(100) not null,
 	constraint id_tienda_pk primary key (id_tienda)
 );
 
 create table seccion (
-	id_seccion integer auto_increment,
-	id_tienda integer not null, 
-	categoria varchar (2) not null, 
+	id_seccion int ,
+	id_tienda int not null, 
+	categoria varchar(2) not null, 
 	constraint id_seccion_pk primary key (id_seccion),
-	constraint id_tienda_fk foreign key (id_tienda) 
+	constraint seccion_id_tienda_fk foreign key (id_tienda) 
 		references tienda (id_tienda),
 	constraint categoria_chk check (categoria in ('DE', 'TH'))
 );
 
 create table producto (
-    codigo_barras char(30) auto_increment,
-	id_seccion integer not null,
-	descripcion varchar (100) not null,
+    codigo_barras char(30) ,
+	id_seccion int not null,
+	descripcion varchar(100) not null,
 	precio float(3) not null,
-	nombre varchar (100) not null,
-	cantidad integer not null,
+	nombre varchar(100) not null,
+	cantidad int not null,
 	constraint codigo_barras_pk primary key (codigo_barras),
-	constraint id_seccion_fk foreign key (id_seccion)
+	constraint prosucto_id_seccion_fk foreign key (id_seccion)
 		references seccion (id_seccion)
 );
 
 create table cliente (
-	id_cliente integer auto_increment,
-	id_tienda integer not null,
+	id_cliente int ,
+	id_tienda int not null,
 	nombre varchar(100) not null,
 	constraint id_cliente_pk primary key (id_cliente),
-	constraint id_tienda_fk foreign key (id_tienda)
-		references tienda (id_tienda)
+	constraint cliente_id_tienda_fk foreign key (id_tienda)
+		references tienda(id_tienda)
 );
 
 create table carrito (
-	id_cliente integer not null,
+	id_cliente int not null,
 	precio float(3) not null,
-	nombre varchar (100) not null,
-	cantidad integer not null,
-    constraint id_cliente_fk foreign key (id_cliente)
+	nombre varchar(100) not null,
+	cantidad int not null,
+	fecha date not null,
+    constraint carrito_id_cliente_fk foreign key (id_cliente)
 		references cliente (id_cliente)
 );
