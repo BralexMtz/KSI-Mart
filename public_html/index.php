8619ae475e596e2a424560594dd70ac0d131cdee
Brayan <cdn.eem_1@comunidad.unam.mx>
2019-03-30 17:50:41 -0600

    Correcion de contraseña y bd en php conexion

--- public_html/index.php
+++ public_html/index.php
@@ -2,8 +2,8 @@
 <?php
 $host = 'mysql';
 $user = 'root';
-$pass = 'rootpassword';
-$database = 'ksiMart';
+$pass = '1q2w3e4r';
+$database = 'KsiMart';
 $conn = new mysqli($host, $user, $pass,$database);
 
 if ($conn->connect_error) {
