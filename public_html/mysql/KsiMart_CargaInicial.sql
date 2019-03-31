12215a73bb46a97a272035bc55ddeda2230bc45f
AndresAmadeus <30989853+AndresAmadeus@users.noreply.github.com>
2019-03-30 19:58:41 -0600

    Carga_Inicial
    Primera parte carga_Inicial

---
+++ public_html/mysql/KsiMart_CargaInicial.sql
@@ -0,0 +1,21 @@
+insert into seccion values(10000, 'DE', 'Lácteos');
+insert into seccion values(20000, 'DE', 'Frutas y verduras');
+insert into seccion values(30000, 'DE', 'Carnes y pescados');
+insert into seccion values(40000, 'DE', 'Salchichonería');
+insert into seccion values(50000, 'DE', 'Cervezas, vinos y licores');
+insert into seccion values(60000, 'DE', 'Farmacia');
+insert into seccion values(70000, 'DE', 'Bebés');
+
+insert into seccion values(80000, 'TH', 'Pantallas');
+insert into seccion values(90000, 'TH', 'Computadoras');
+insert into seccion values(100000, 'TH', 'Electrodomésticos');
+insert into seccion values(110000, 'TH', 'Celulares');
+
+insert into producto values(10001, 10000, 'Leche Alpura Clásica entera 1 L', 19.80, 400, 'La leche entera es primordial en tu lista de abarrotes ya que es una fuente de vitaminas y proteínas que puedes incluir en las mañanas para tener una alimentación balanceada. La leche Alpura Clásica, en su presentación de 1 litro adicionada con vitaminas A y D que son esenciales para tener una buena salud y un sistema inmunológico fuerte, puede formar parte de una alimentación balanceada porque es un producto lácteo muy nutritivo. ');
+
+insert into producto values(, , '', , , '');
+insert into producto values(, , '', , , '');
+insert into producto values(, , '', , , '');
+insert into producto values(, , '', , , '');
+insert into producto values(, , '', , , '');
+insert into producto values(, , '', , , '');
--- public_html/mysql/KsiMart_MYSQL.sql
+++ public_html/mysql/KsiMart_MYSQL.sql
@@ -10,10 +10,10 @@ create table seccion (
 create table producto (
     codigo_barras integer auto_increment,
 	id_seccion integer not null,
-	descripcion varchar (100) not null,
-	precio float(3) not null,
 	nombre varchar (100) not null,
+	precio float(3) not null,
 	cantidad integer not null,
+	descripcion varchar (200) not null,
 	constraint codigo_barras_pk primary key (codigo_barras),
 	constraint id_seccion_fk foreign key (id_seccion)
 		references seccion (id_seccion)
