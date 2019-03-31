8d3f9ad73af88678298ae8d3747c508f80fab86a
AndresAmadeus <30989853+AndresAmadeus@users.noreply.github.com>
2019-03-30 20:29:21 -0600

    Frutas

--- public_html/mysql/KsiMart_CargaInicial.sql
+++ public_html/mysql/KsiMart_CargaInicial.sql
@@ -19,3 +19,50 @@ insert into producto values(10004, 10000, 20.90, 500, 'Leche Lala entera 1 L', '
 insert into producto values(10005, 10000, 14.40, 340, 'Producto lácteo Forti Leche 7 Plus 1 L', 'Consumir lácteos es muy importante en la dieta diaria por su contenido de calcio; además, algunos de ellos están adicionados con vitaminas y minerales que complementan una sana alimentación. La fórmula láctea de Fortileche, en su presentación de 1 litro, cuenta con un valor nutricional agregado porque viene enriquecida con vitamina A, B6,D, ácido fólico, niacina y hierro; además pueden hacerse ricos batidos con las frutas de temporada que complementarán la alimentación diaria.');
 insert into producto values(10006, 10000, 16.90, 410, 'Leche Great Value entera 1 L', 'Si estás buscando alimentos ricos en calcio, tenemos para ti un producto lácteo que te encantará. Llévate la leche entera de la marca Great Value que tenemos en presentación de 1 litro y disfruta de este alimento rico en calcio que además es delicioso. Disfruta todos los beneficios de la leche natural en un rico vaso de leche por las mañanas o a cualquier hora del día. ');
 insert into producto values(10007, 10000, 18.00, 290, 'Leche San Marcos entera 1 L', 'Incluye en tu lista de lácteos la leche entera San Marcos, con propiedades como vitaminas A y D, ideales para el desayuno saludable de toda la familia. Logra una alimentación balanceada sin perder el rico sabor y cremosidad de tu leche favorita. Los productos lácteos de San Marcos son ideales para integrar en tu despensa básica, ya que cuentan con una gran variedad de lácteos y derivados que podrás combinar con fruta, café o pastas. ');
+
+insert into producto values(20001, 20000, 28.90, 50, 'Plátano macho por kilo', 'En Ksi-Mart tienda en línea tenemos para ti el nutritivo plátano macho, sin duda una fruta típica que no puede faltar en los hogares. Este alimento está lleno de beneficios para tu salud, contiene fibra, vitaminas para la piel, es fuente de potasio para los huesos y controla la hipertensión. Puedes comerlo todos los días para que goces de una buena salud, recuerda disfrutarlo sobretodo cocinado. Hornéalo o prepáralo con muy poco aceite, perfecto para postres como plátanos fritos con leche condensada o para acompañar el plato fuerte. ');
+insert into producto values(20002, 20000, 19.90, 90, 'Plátano Chamitos orgánico por kilo', 'El plátano orgánico por kilo es un producto perfecto para disfrutar a lado de tu familia ya sea en platillos de cocina como arroz con plátano, postres como el pay de plátano o simplemente disfrutar una pieza del increíble sabor que un producto orgánico te brinda, ya que gracias a su cosecha libre de pesticidas posee un sabor único. No olvides comprar todos los ingredientes que necesitas para poder preparar platillos naturales y saludables para tu familia.');
+insert into producto values(20003, 20000, 69.00, 40, 'Nectarina por kilo', 'Dale a tu familia productos naturales, frescos y con un delicioso sabor, adquiere en Ksi-Mart tienda en línea y cómela sola o disfrútala en conserva, jalea, mermelada o en confitura, sin duda es el postre perfecto y nutritivo que a todos encantará. Además esta fruta cuenta con múltiples propiedades benéficas para la salud, contiene vitaminas A, B3, C y ácido fólico, que contribuyen a la producción de colágeno, un componente muy importante para la piel; también ayuda a reducir los niveles de colesterol, por sus nutrientes es ideal para incluirla en tu dieta diaria.');
+insert into producto values(20004, 20000, 84.00, 40, 'Uva blanca sin semilla por kilo', 'En Ksi-Mart tienda en línea nos gusta que lleves una vida sana y nutritiva, por eso traemos para ti la uva blanca sin semilla. Una fruta fresca que debe estar incluida en tu dieta diaria pues aporta múltiples beneficios a tu organismo. Las uvas tienen diferentes propiedades que favorecen a la salud pues ayudan a prevenir el cáncer, lesiones arteriales y neurológicas; tienen un alto contenido en vitaminas C, K, y B6. Puedes comerlas solas o en ensaladas con algunos otros complementos que aporten más nutrientes a tu cuerpo. ');
+insert into producto values(20005, 20000, 24.90, 60, 'Melón chino por kilo', 'Si quieres preparar un delicioso coctel de frutas, en Walmart tenemos para ti los productos más frescos y de gran calidad, como el melón chino que te brinda un sabor dulce y es ideal para integrar en tu dieta diaria gracias a sus propiedades nutrimentales. Contiene grandes cantidades de vitamina C que te ayuda a hidratar tu piel evitando imperfecciones y protege tu sistema inmunológico. Además, es un excelente diurético que limpia tu sistema digestivo y elimina la acidez provocada por el exceso de grasas, así que no dudes en llevarlo a tu mesa y disfrutarlo a la hora que quieras. ');
+insert into producto values(20006, 20000, 59.00, 70, 'Manzana granny smith por kg', 'Una fruta fresca con un sabor ácido es la granny Smith. Descubre la experiencia que esta manzana crujiente la despensa de tu hogar. Son muy buenas para preparar cualquier tipo de receta de cocina incluyendo los platillos para hornear. No lo pienses más y crea incluso un puré. Esta manzana es alta en fibra y antioxidantes además de ser baja en calorías y grasas, son aptas para una buena alimentación. Pruébala. ');
+insert into producto values(20007, 20000, 75.00, 80, 'Limón eureka por kilo', 'Refréscate de manera natural con una deliciosa agua con chía, pepino y limón eureka, que a diferencia del limón tradicional, tiene un sabor menos agrio y por su esencia también es muy útil para que los agregues en guisos o postres. Este tipo de cítricos son ricos en vitamina C, la cual te ayuda a prevenir enfermedades respiratorias, además fortalece tu sistema inmunológico y contrarresta la presencia de bacterias en tu cuerpo. ');
+insert into producto values(20006, 20000, 17.90, 60, 'Mandarina por kilo', 'Una de las frutas de temporada más esperadas son las mandarinas, ya que de entre las frutas y verduras, son las que tienen un elevado contenido de vitamina C además de ser un cítrico de sabor dulce y delicado. Consume jugo de mandarina como un antigripal natural y prepárate para esta época decembrina en la que las defensas deben estar altas con la ayuda de esta fruta fresca. ');
+insert into producto values(20007, 20000, 14.90, 70, 'Naranja valencia por kilo', 'Comenzar el día con un vaso de jugo es casi una tradición en muchos hogares, pues las propiedades de los cítricos son incuestionables. Por ello, la Naranja Valencia es un alimento que siempre debes tener a la mano. Además, pese a que comúnmente se sirve de este modo, su uso puede aportarte una gran cantidad de soluciones también al momento de cocinar.La Naranja Valencia cuenta con propiedades nutricionales que le permiten ayudar a contrarrestar problemas que van desde trastornos estomacales o dolor de cabeza hasta estrés y depresión. Además de su alto contenido de vitamina C, posee minerales como calcio, fósforo, hierro y magnesio. Por si fuera poco, con ella puedes eliminar bacterias y dar un sabor más agradable a alimentos como el pollo o el pato, entre muchos otros.');
+
+insert into producto values(20006, 20000, , , '', '');
+insert into producto values(20007, 20000, , , '', '');
+insert into producto values(20006, 20000, , , '', '');
+insert into producto values(20007, 20000, , , '', '');
+insert into producto values(20006, 20000, , , '', '');
+insert into producto values(20007, 20000, , , '', '');
+insert into producto values(20006, 20000, , , '', '');
+insert into producto values(20007, 20000, , , '', '');
+
+
+insert into producto values(3000, 30000, , , '', '');
+insert into producto values(3000, 30000, , , '', '');
+insert into producto values(3000, 30000, , , '', '');
+insert into producto values(3000, 30000, , , '', '');
+insert into producto values(3000, 30000, , , '', '');
+insert into producto values(3000, 30000, , , '', '');
+insert into producto values(3000, 30000, , , '', '');
+
+
+insert into producto values(4000, 40000, , , '', '');
+insert into producto values(4000, 40000, , , '', '');
+insert into producto values(4000, 40000, , , '', '');
+insert into producto values(4000, 40000, , , '', '');
+insert into producto values(4000, 40000, , , '', '');
+insert into producto values(4000, 40000, , , '', '');
+insert into producto values(4000, 40000, , , '', '');
+
+
+
+insert into producto values(5000, 50000, , , '', '');
+insert into producto values(5000, 50000, , , '', '');
+insert into producto values(5000, 50000, , , '', '');
+insert into producto values(5000, 50000, , , '', '');
+insert into producto values(5000, 50000, , , '', '');
+insert into producto values(5000, 50000, , , '', '');
+insert into producto values(5000, 50000, , , '', '');
---
+++ public_html/mysql/para_Sofi.sql
@@ -0,0 +1,33 @@
+insert into producto values(8000, 80000, , , '', '');
+insert into producto values(8000, 80000, , , '', '');
+insert into producto values(8000, 80000, , , '', '');
+insert into producto values(8000, 80000, , , '', '');
+insert into producto values(8000, 80000, , , '', '');
+insert into producto values(8000, 80000, , , '', '');
+insert into producto values(8000, 80000, , , '', '');
+
+insert into producto values(9000, 90000, , , '', '');
+insert into producto values(9000, 90000, , , '', '');
+insert into producto values(9000, 90000, , , '', '');
+insert into producto values(9000, 90000, , , '', '');
+insert into producto values(9000, 90000, , , '', '');
+insert into producto values(9000, 90000, , , '', '');
+insert into producto values(9000, 90000, , , '', '');
+
+
+insert into producto values(10000, 100000, , , '', '');
+insert into producto values(10000, 100000, , , '', '');
+insert into producto values(10000, 100000, , , '', '');
+insert into producto values(10000, 100000, , , '', '');
+insert into producto values(10000, 100000, , , '', '');
+insert into producto values(10000, 100000, , , '', '');
+insert into producto values(10000, 100000, , , '', '');
+
+
+insert into producto values(11000, 110000, , , '', '');
+insert into producto values(11000, 110000, , , '', '');
+insert into producto values(11000, 110000, , , '', '');
+insert into producto values(11000, 110000, , , '', '');
+insert into producto values(11000, 110000, , , '', '');
+insert into producto values(11000, 110000, , , '', '');
+insert into producto values(11000, 110000, , , '', '');
