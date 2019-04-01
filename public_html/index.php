afe59332bda30ad9daa94d6f76d30a2712d4b0d9
Brayan <cdn.eem_1@comunidad.unam.mx>
2019-03-31 23:11:09 -0600

    Algunas modificaciones del front-end

--- public_html/index.php
+++ public_html/index.php
@@ -1,49 +1,46 @@
 <!DOCTYPE html>
-<html lang="es" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>KSI-Mart</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    </head>
-    <body>
-        <h1>KSI Mart</h1>
-        <hr>
-        <br>
-        Bienvenido
-        <div class="card-deck">
-            <div class="card">
-                <img src="media/Leche_Alp.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                      <h5 class="card-title">nombre del producto</h5>
-                      <p class="card-text">DESCRIPCION DEL PRODUCTO This content is a little bit longer.</p>
-                      <p class="card-text"><small class="text-muted">Numero de catalogo: 10002</small></p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Precio: $ 200.00</small>
-                </div>
-            </div>
-            <div class="card">
-                <img src="media/Leche_Alp.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                      <h5 class="card-title">nombre del producto</h5>
-                      <p class="card-text">DESCRIPCION DEL PRODUCTO This content is a little bit longer.</p>
-                      <p class="card-text"><small class="text-muted">Numero de catalogo: 10002</small></p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Precio: $ 200.00</small>
-                </div>
-            </div>
-            <div class="card">
-                <img src="media/Leche_Alp.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                      <h5 class="card-title">nombre del producto</h5>
-                      <p class="card-text">DESCRIPCION DEL PRODUCTO This content is a little bit longer.</p>
-                      <p class="card-text"><small class="text-muted">Numero de catalogo: 10002</small></p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Precio: $ 200.00</small>
-                </div>
-            </div>
-    </div>
-    </body>
+<html>
+<head>
+	<title>KSI-MART</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="CSS/estilo.css">
+	<link rel="icon" type="image/png" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9N6cNbzR2yqSprlC_il1kAlknV76am1m6bq-AcRO_4HvN7AHf">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+</head>
+<body>
+<nav>
+	<table>
+		<tr >
+			<td><a href="Carrito.html"><img src="IMG/carrito.png" class="car"></a></td>
+			<td><a class="botón" href="index.php" target="_self">Inicio</a></td>
+			<td><a class="botón" href="Despensa.html" target="_self">Despensa</a></td>
+			<td><a class="botón" href="TecyHogar.html" target="_self">Tecnología y Hogar</a></td>
+
+			<td class="uno"></td>
+
+		</tr>
+	</table>
+
+</nav>
+<section>
+	<div class="polaroidContenedor RotarIzq">
+    	<img class="zine" src="IMG/ksi.jpg" alt="Imagen Polaroid con CSS">
+	</div>
+
+</section>
+<form class="w-25 form-group auto float-right">
+    <label for="exampleInputEmail1">Nombre completo</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar nombre completo">
+    <small id="emailHelp" class="form-text text-muted">Solo en caso de querer realizar compras</small>
+  <button type="submit" class="btn btn-success form-control">Submit</button>
+</form>
+
+<footer class="curri">
+			<strong>Contacto</strong>
+			<BR>
+			<a href="https://www.jasoft.org/Blog/post/Efecto-Polaroid-y-giros-en-fotografias-con-CSS.aspx" style="float: right;color: white;"><img class="curri" src="IMG/polaroid.png">Referencias</a>
+
+
+		</footer>
+</body>
 </html>
---
+++ public_html/prueba.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>KSI-Mart</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>KSI Mart</h1>
+        <hr>
+        <br>
+        Bienvenido
+        <div class="card-deck">
+            <div class="card">
+                <img src="media/Leche_Alp.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                      <h5 class="card-title">nombre del producto</h5>
+                      <p class="card-text">DESCRIPCION DEL PRODUCTO This content is a little bit longer.</p>
+                      <p class="card-text"><small class="text-muted">Numero de catalogo: 10002</small></p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Precio: $ 200.00</small>
+                </div>
+            </div>
+            <div class="card">
+                <img src="media/Leche_Alp.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                      <h5 class="card-title">nombre del producto</h5>
+                      <p class="card-text">DESCRIPCION DEL PRODUCTO This content is a little bit longer.</p>
+                      <p class="card-text"><small class="text-muted">Numero de catalogo: 10002</small></p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Precio: $ 200.00</small>
+                </div>
+            </div>
+            <div class="card">
+                <img src="media/Leche_Alp.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                      <h5 class="card-title">nombre del producto</h5>
+                      <p class="card-text">DESCRIPCION DEL PRODUCTO This content is a little bit longer.</p>
+                      <p class="card-text"><small class="text-muted">Numero de catalogo: 10002</small></p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Precio: $ 200.00</small>
+                </div>
+            </div>
+    </div>
+    </body>
+</html>
